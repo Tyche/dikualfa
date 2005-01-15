465fb1c7e279b760129054b3819c890260e104f4
Jon A. Lambert <jlambert@oh.rr.com>
2005-01-15 23:13:39 +0000

    New header to gather declarations together.

---
+++ prototypes.h
@@ -0,0 +1,26 @@
+/*! \file prototypes.h
+  The intent of this header file is to provide explicit declarations
+  for public functions.
+
+ \author Jon A. Lambert
+ \date 01/15/2005
+ \version 0.1
+ \remarks
+  This source code copyright (C) 2005 by Jon A. Lambert
+  All rights reserved.
+
+  Released under the same terms of DikuMud
+*/
+
+#ifndef PROTOTYPES_H
+#define PROTOTYPES_H
+
+
+/* weather.c */
+extern void night_watchman (void);
+
+/* weather.c */
+extern void weather_and_time (int mode);
+
+
+#endif
